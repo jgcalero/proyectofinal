aecd22fe11ebba97e851da15e21d71e9ae1e8d99
Jonathan Calero Armijos <132386033+jgcalero@users.noreply.github.com>
2024-02-18 17:19:57 -0500

    Update App.scala

--- App.scala
+++ App.scala
@@ -5,10 +5,11 @@ import java.io.File
 import org.nspl.*
 import org.nspl.awtrenderer.*
 import org.nspl.data.HistogramData
-
 import java.util.regex.Pattern.matches
 import scala.collection.immutable.ListMap
-
+import java.time.LocalDate
+import java.time._
+import java.time.format.DateTimeFormatter
 
 implicit object MyFormat extends DefaultCSVFormat {
   override val delimiter = ';'
@@ -26,44 +27,60 @@ object App {
     reader2.close()
 
     //println(contentFile.take(1))      //imprime las cabeceras del archivo
-    //println(s"Filas: ${contentFile.length} y Columnas: ${contentFile(1).keys.size}") //Imprime # de filas y columnas
-    //println(s"Filas: ${contentFile2.length} y Columnas: ${contentFile2(1).keys.size}")
+    println(s"Filas: ${contentFile.length} y Columnas: ${contentFile(1).keys.size}") //Imprime # de filas y columnas
+    //println(contentFile2.take(1))
+    println(s"Filas: ${contentFile2.length} y Columnas: ${contentFile2(1).keys.size}")
 
     println("PREGUNTAS DE DATOS ESTADISTICOS DESCRIPTIVOS")
-    println(s"Total de partidos jugados: ${getTotalPartidos(contentFile)} donde el resultado fue diferente de empate")
+    println(s"Total de partidos jugados: ${TotalPartidos(contentFile)} donde el resultado fue diferente de empate")
     println(s"Media de goles por partido: ${mediaGolesporPartido(contentFile)}")
     println(s"Estadio capacidad maxima: ${estadioMaxCapacidad(contentFile)}")
     println(s"Estadio capacidad minima: ${estadioMinCapacidad(contentFile)}")
     println(s"Pais con más mundiales: ${maxCampeon(contentFile)}")
+    println(s"desviación estandar: ${stdDevTeams(contentFile2)}")
+    println(s"Total de jugadores en los mundiales: ${totalJugadores(contentFile2)}")
+    println(s"Media del número de la camiseta de los jugadores: ${mediaNumeroCamiseta(contentFile2)}")
 
+    def stdDevTeams(torneos: List[Map[String, String]]): Double = {
+      // Convierte la lista de mapas en una lista de tuplas
+      val equipos = torneos.flatMap { row =>
+        row.get("tournaments_teams").map(_.toInt).map((row("tournaments_name"), _))
+      }
+      val equipoValido = equipos.filter(_._2 > 0)       // Filtra torneos equipos válidos
+      val equipoMedio = equipoValido.map(_._2).sum.toDouble / equipoValido.size
+      // Calcula las desviaciones al cuadrado de la media.
+      val desviacionStandar = equipoValido.map { case (_, teamCount) =>
+        Math.pow(teamCount - equipoMedio, 2)
+      }
+      val varianza = desviacionStandar.sum / equipoValido.size //Calcula la varianza
+      Math.sqrt(varianza)     //Calcula y retornala desviación estandar
+    }
 
     //Función para obtener el total de partidos que tuvieron un resultado diferente de empate
-    def getTotalPartidos(data: List[Map[String, String]]): Int = {
+    def TotalPartidos(data: List[Map[String, String]]): Int = {
       // Filtra las filas con datos faltantes o en blanco
-      val filteredData = data.filter(row =>
+      val filtraData = data.filter(row =>
         row.contains("matches_match_id") &&
           row.contains("matches_result")
       )
-      filteredData.count(row => row("matches_result") != "draw")
+      filtraData.count(row => row("matches_result") != "draw")
       //Cuenta el número de filas que el resultado es diferente de empate
     }
-    getTotalPartidos(contentFile)
 
     //Promedio de goles por partido
     def mediaGolesporPartido(data: Seq[Map[String, String]]): Double = {
       // Filtra los datos que no tienen goles en información
-      val filteredData = data.filter(row => row.get("matches_home_team_score").isDefined && row.get("matches_away_team_score").isDefined)
+      val filtraData = data.filter(row => row.get("matches_home_team_score").isDefined && row.get("matches_away_team_score").isDefined)
       //Convierte la data a un tipo de dato double
-      val golesData = filteredData.map(row => {
-        val homeTeamGoals = row("matches_home_team_score").toDouble
-        val awayTeamGoals = row("matches_away_team_score").toDouble
-        (homeTeamGoals, awayTeamGoals)
+      val golesData = filtraData.map(row => {
+        val golLocales = row("matches_home_team_score").toDouble
+        val golVisitante = row("matches_away_team_score").toDouble
+        (golLocales, golVisitante)
       })
       //println(goalsData)
       //Calcula el número total de goles y partidos
       val totalGoles = golesData.map(_.toDouble + _.toDouble).sum
       val totalPartidos = golesData.size
-
       // Calcula y retorna la media de goles por partido
       totalGoles.toDouble / totalPartidos
     }
@@ -76,12 +93,25 @@ object App {
     def estadioMinCapacidad(data: List[Map[String, String]]): (Int, String) =
       data.map(tuple3 => (tuple3("stadiums_stadium_capacity").toInt, tuple3("stadiums_stadium_name"))).min
 
-    //Función para encontrar el maximo campeón de los mundiiles
+    //Función para encontrar el maximo campeón de los mundiales
     def maxCampeon(campeonMundial: List[Map[String, Any]]): (String, Int) = {
       val campeonesmundiales = campeonMundial.groupBy(campeon => campeon("tournaments_winner").toString)
       val campeonPais = campeonesmundiales.mapValues(_.size)
       val campeon = campeonPais.maxBy(_._2)
       (campeon._1, campeon._2)
+    }
+
+    //función para obtener el número de jugadores en los mundiales
+    def totalJugadores(data: List[Map[String, String]]): Int = {
+      //Cuenta el número distinto de jugadores
+      data.map(_.get("squads_player_id").get).distinct.size
+    }
+
+    //función para calcular el premdio del número de la camiseta de los jugadores
+    def mediaNumeroCamiseta(players: List[Map[String, String]]): Double = {
+      val jugadorSinNumero = players.filter(_.get("squads_shirt_number").isDefined)  //Filtra jugadores sin # en la camiseta
+      val numEnCamisera = jugadorSinNumero.map(_.get("squads_shirt_number").get.toInt)   //Convierte el # de camiseta a entero
+      numEnCamisera.sum.toDouble / numEnCamisera.size     //Calcula y retorna la media
     }
 
 
